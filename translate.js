1d9af64e81c4b0fad5bdf6a1dfc7e6876687d80c
BeCare666 <becare.fr.ge@gmail.com>
2025-08-20 13:02:31 +0100

    cv

--- translate.js
+++ translate.js
@@ -1,30 +1,86 @@
 
 document.addEventListener("DOMContentLoaded", () => {
+    // Ouvrir modal au clic
+    document.getElementById("openLangModalBtn").addEventListener("click", () => {
+        document.getElementById("langModal").style.display = "flex";
+        document.getElementById("INDEXID5").innerHTML = i18next.t("INDEXID5");
+        document.getElementById("INDEXID6").innerHTML = i18next.t("INDEXID6");
+    });
 
-const resources = {
-    en: {
-        translation: {
-            "INDEXID1": `SUPPORTING AFRICA’S CATCH-UP
+    // Fermer modal au clic sur la croix
+    document.getElementById("closeLangModalBtn").addEventListener("click", () => {
+        document.getElementById("langModal").style.display = "none";
+    });
+
+    // Fermer modal si clic en dehors de la boite blanche
+    document.getElementById("langModal").addEventListener("click", (e) => {
+        if (e.target === document.getElementById("langModal")) {
+            document.getElementById("langModal").style.display = "none";
+        }
+    });
+
+    // Changement de langue
+    document.addEventListener("DOMContentLoaded", () => {
+        let savedLang = localStorage.getItem("selectedLang") || "fr"; // fr par défaut
+
+        const langFlags = {
+            fr: "https://flagcdn.com/w40/fr.png",
+            en: "https://flagcdn.com/w40/gb.png",
+        };
+
+        i18next.changeLanguage(savedLang, () => {
+            updateContent();
+            document.getElementById("currentFlag").src = langFlags[savedLang];
+        });
+    });
+
+    document.querySelectorAll(".langOption").forEach(option => {
+        option.addEventListener("click", function () {
+            let lang = this.getAttribute("data-lang");
+            let imgSrc = this.querySelector("img").src;
+
+            localStorage.setItem("selectedLang", lang);
+             document.getElementById("langModal").style.display = "none";
+            location.reload();
+            i18next.changeLanguage(lang, () => {
+                updateContent();
+                document.getElementById("langModal").style.display = "none";
+            });
+
+            document.getElementById("currentFlag").src = imgSrc;
+
+        });
+    });
+
+    document.addEventListener("DOMContentLoaded", function () {
+        setTimeout(() => {
+            // document.getElementById("loading").style.display = "none"
+        }, 7000);
+    });
+    const resources = {
+        en: {
+            translation: {
+                "INDEXID1": `SUPPORTING AFRICA’S CATCH-UP
                                 AND GEO-ECONOMIC EXPANSION
                                 TOWARDS 2100`,
-            "INDEXID2": `Click here to learn more`,
-            "INDEXID2X": `News`,
-            "INDEXID2XX": `News`,
-             "INDEXID2XXY": `📢 News`,
-            "INDEXID3": `<strong>GR-AfCFTA 2100 :</strong> A Strategic
+                "INDEXID2": `Click here to learn more`,
+                "INDEXID2X": `GR AFCFTA 2100 INFOS`,
+                "INDEXID2XX": `GR AFCFTA 2100 INFOS`,
+                "INDEXID2XXY": `📢 GR AFCFTA 2100 INFOS`,
+                "INDEXID3": `<strong>GR-AfCFTA 2100 :</strong> A Strategic
                                 Support Plan for Africa’s Catch-Up
                                 and Geo-Economic Expansion
                                 towards 2100
                                 `,
-            "INDEXID5": `Contact us`,
-            "INDEXID6": `Download`,
-            "INDEXID7": `Hours`,
-            "INDEXID8": `Minutes`,
-            "INDEXID9": `Seconds`,
-            "INDEXID10": `Days`,
-            "INDEXID7": `Hours`,
-            "INDEXID8": `Minutes`,
-            "INDEXID11": ` <div class="flag"><img src="https://flagcdn.com/w40/cd.png" alt="RDC" />RDC</div>
+                "INDEXID5": `Contact us`,
+                "INDEXID6": `Download`,
+                "INDEXID7": `Hours`,
+                "INDEXID8": `Minutes`,
+                "INDEXID9": `Seconds`,
+                "INDEXID10": `Days`,
+                "INDEXID7": `Hours`,
+                "INDEXID8": `Minutes`,
+                "INDEXID11": ` <div class="flag"><img src="https://flagcdn.com/w40/cd.png" alt="RDC" />RDC</div>
                             <div class="flag"><img src="https://flagcdn.com/w40/td.png" alt="Tchad" />Tchad</div>
                             <div class="flag"><img src="https://flagcdn.com/w40/ga.png" alt="Gabon" />Gabon</div>
                             <div class="flag"><img src="https://flagcdn.com/w40/ci.png" alt="Côte d'Ivoire" />Côte
@@ -53,35 +109,36 @@ const resources = {
                             <div class="flag"><img src="https://flagcdn.com/w40/bj.png" alt="Benin" />Benin</div>
                             <div class="flag"><img src="https://flagcdn.com/w40/bf.png" alt="Burkina Faso" /> Burkina Faso</div>
                             <div class="flag"><img src="https://flagcdn.com/w40/ne.png" alt="Niger" />Niger</div>`,
-            // Nouveaux textes pour actualités
-            "noNews": "No news available.",
-            "noPdf": "No PDF available",
-            "alreadyVoted": "You have already voted!",
-            "downloadPdf": "Download PDF",
-            "emailRequired": "Email is required",
-            "emailInvalid": "Invalid email",
-            "alreadyDownloaded": "You have already downloaded this document",
-            "downloading": "Downloading...",
-            "downloaded": "Your PDF has been downloaded",
+                // Nouveaux textes pour actualités
+                "noNews": "No news available.",
+                "noPdf": "No PDF available",
+                "alreadyVoted": "You have already voted!",
+                "downloadPdf": "Download PDF",
+                "emailRequired": "Email is required",
+                "emailInvalid": "Invalid email",
+                "alreadyDownloaded": "You have already downloaded this document",
+                "downloading": "Downloading...",
+                "downloaded": "Your PDF has been downloaded",
 
-        }
-    },
+            }
+        },
 
-    fr: {
-        translation: {
-            "INDEXID1": `ACCOMPAGNER LE RATTRAPAGE ET L’EXPANSION GÉOÉCONOMIQUE DE L’AFRIQUE À L’HORIZON 2100.`,
-            "INDEXID2": `CLiquez ici pour en savoir plus`,
-            "INDEXID2X": `Les actualités`,
-            "INDEXID2XX": `Les actualités`,
-             "INDEXID2XXY": `📢 Actualités`,
-            "INDEXID3": `<strong>GR-AfCFTA 2100 :</strong> <b>Un Plan d’appui stratégique pour le rattrapage et l’expansion géoéconomique de l’Afrique à l’horizon 2100</b>`,
-            "INDEXID5": `Contactez-nous`,
-            "INDEXID6": ` Télécharger`,
-            "INDEXID7": `Heures`,
-            "INDEXID8": `Minutes`,
-            "INDEXID9": `Sécondes`,
-            "INDEXID10": `Jours`,
-            "INDEXID11": `                            <div class="flag"><img src="https://flagcdn.com/w40/cd.png" alt="RDC" />RDC</div>
+        fr: {
+            translation: {
+                "INDEXID1": `ACCOMPAGNER LE RATTRAPAGE ET L’EXPANSION GÉOÉCONOMIQUE DE L’AFRIQUE À L’HORIZON 2100.`,
+                "INDEXID2": `CLiquez ici pour en savoir plus`,
+                "INDEXID2X": `GR AFCFTA 2100 INFOS`,
+                "INDEXID2XW": `GR AFCFTA 2100 INFOS`,
+                "INDEXID2XX": `GR AFCFTA 2100 INFOS`,
+                "INDEXID2XXY": `GR AFCFTA 2100 INFOS`,
+                "INDEXID3": `<strong>GR-AfCFTA 2100 :</strong> <b>Un Plan d’appui stratégique pour le rattrapage et l’expansion géoéconomique de l’Afrique à l’horizon 2100</b>`,
+                "INDEXID5": `Contactez-nous`,
+                "INDEXID6": ` Télécharger`,
+                "INDEXID7": `Heures`,
+                "INDEXID8": `Minutes`,
+                "INDEXID9": `Sécondes`,
+                "INDEXID10": `Jours`,
+                "INDEXID11": `                            <div class="flag"><img src="https://flagcdn.com/w40/cd.png" alt="RDC" />RDC</div>
                             <div class="flag"><img src="https://flagcdn.com/w40/td.png" alt="Tchad" />Tchad</div>
                             <div class="flag"><img src="https://flagcdn.com/w40/ga.png" alt="Gabon" />Gabon</div>
                             <div class="flag"><img src="https://flagcdn.com/w40/ci.png" alt="Côte d'Ivoire" />Côte
@@ -112,99 +169,45 @@ const resources = {
                                 Faso</div>
                             <div class="flag"><img src="https://flagcdn.com/w40/ne.png" alt="Niger" />Niger</div>`,
 
-            // Nouveaux textes pour actualités
-            "noNews": "Aucune actualité disponible.",
-            "noPdf": "Pas de PDF",
-            "alreadyVoted": "Vous avez déjà voté !",
-            "downloadPdf": "Télécharger le PDF",
-            "emailRequired": "Email requis",
-            "emailInvalid": "Email invalide",
-            "alreadyDownloaded": "Vous avez déjà téléchargé ce document",
-            "downloading": "Téléchargement en cours...",
-            "downloaded": "Votre PDF a été téléchargé",
-            "error": "Erreur",
+                // Nouveaux textes pour actualités
+                "noNews": "Aucune actualité disponible.",
+                "noPdf": "Pas de PDF",
+                "alreadyVoted": "Vous avez déjà voté !",
+                "downloadPdf": "Télécharger le PDF",
+                "emailRequired": "Email requis",
+                "emailInvalid": "Email invalide",
+                "alreadyDownloaded": "Vous avez déjà téléchargé ce document",
+                "downloading": "Téléchargement en cours...",
+                "downloaded": "Votre PDF a été téléchargé",
+                "error": "Erreur",
+            }
         }
-    }
-};
-
-// Init i18next
-i18next
-    .use(i18nextBrowserLanguageDetector)
-    .init({
-        resources,
-        fallbackLng: "fr",
-        debug: false
-    }, function () {
-        updateContent();
-    });
-
-// Fonction pour mettre à jour le contenu
-function updateContent() {
-     document.getElementById("INDEXID1").innerHTML = i18next.t("INDEXID1");
-    document.getElementById("INDEXID2").innerHTML = i18next.t("INDEXID2");
-    document.getElementById("INDEXID2X").innerHTML = i18next.t("INDEXID2X");
-    document.getElementById("INDEXID2XX").innerHTML = i18next.t("INDEXID2XX");
-     document.getElementById("INDEXID2XXY").innerHTML = i18next.t("INDEXID2XXY");
-    document.getElementById("INDEXID3").innerHTML = i18next.t("INDEXID3");
-    document.getElementById("INDEXID11").innerHTML = i18next.t("INDEXID11");
-
-}
-
-// Ouvrir modal au clic
-document.getElementById("openLangModalBtn").addEventListener("click", () => {
-    document.getElementById("langModal").style.display = "flex";
-    document.getElementById("INDEXID5").innerHTML = i18next.t("INDEXID5");
-    document.getElementById("INDEXID6").innerHTML = i18next.t("INDEXID6");
-});
-
-// Fermer modal au clic sur la croix
-document.getElementById("closeLangModalBtn").addEventListener("click", () => {
-    document.getElementById("langModal").style.display = "none";
-});
-
-// Fermer modal si clic en dehors de la boite blanche
-document.getElementById("langModal").addEventListener("click", (e) => {
-    if (e.target === document.getElementById("langModal")) {
-        document.getElementById("langModal").style.display = "none";
-    }
-});
-
-// Changement de langue
-document.addEventListener("DOMContentLoaded", () => {
-    let savedLang = localStorage.getItem("selectedLang") || "fr"; // fr par défaut
-
-    const langFlags = {
-        fr: "https://flagcdn.com/w40/fr.png",
-        en: "https://flagcdn.com/w40/gb.png",
     };
 
-    i18next.changeLanguage(savedLang, () => {
-        updateContent();
-        document.getElementById("currentFlag").src = langFlags[savedLang];
-    });
-});
-
-document.querySelectorAll(".langOption").forEach(option => {
-    option.addEventListener("click", function () {
-        let lang = this.getAttribute("data-lang");
-        let imgSrc = this.querySelector("img").src;
-
-        localStorage.setItem("selectedLang", lang);
-
-        i18next.changeLanguage(lang, () => {
+    // Init i18next
+    i18next
+        .use(i18nextBrowserLanguageDetector)
+        .init({
+            resources,
+            fallbackLng: "fr",
+            debug: false
+        }, function () {
             updateContent();
-            document.getElementById("langModal").style.display = "none";
         });
 
-        document.getElementById("currentFlag").src = imgSrc;
-        location.reload();
-    });
-});
+    // Fonction pour mettre à jour le contenu
+    function updateContent() {
+        document.getElementById("INDEXID1").innerHTML = i18next.t("INDEXID1");
+        document.getElementById("INDEXID2").innerHTML = i18next.t("INDEXID2");
+        document.getElementById("INDEXID2X").innerHTML = i18next.t("INDEXID2X");
 
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(() => {
-        // document.getElementById("loading").style.display = "none"
-    }, 7000);
-}); 
+        document.getElementById("INDEXID2XX").innerHTML = i18next.t("INDEXID2XX");
+        document.getElementById("INDEXID2XXY").innerHTML = i18next.t("INDEXID2XXY");
+        document.getElementById("INDEXID3").innerHTML = i18next.t("INDEXID3");
+        document.getElementById("INDEXID11").innerHTML = i18next.t("INDEXID11");
+
+    }
+
+
 
 });
